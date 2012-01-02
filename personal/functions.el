a694692a4667f72d54aeb16145a08add6f12ae28
Jeff Aigner <enumeration@gmail.com>
2012-01-02 10:17:24 -0600

    Made some C++ specific mods to personal config

--- personal/functions.el
+++ personal/functions.el
@@ -1,5 +1,18 @@
 
+;; C++ modifications
+(global-set-key [(f9)] 'compile)
+(setq compilation-window-height 14)
+(c-toggle-hungry-state 1)
 
+(setq compilation-finish-function
+      (lambda (buf str)
+        (if (string-match "exited abnormally" str)
+            ;; errors happened
+            (message "compilation errors, press C-x ` to visit")
+
+          ;; no errors
+          (run-at-time 1.0 nil 'delete-windows-on buf)
+          (message "NO COMPILATIONS ERRORS!"))))
 
 ;; google weather
 (require 'google-weather)
@@ -7,6 +20,7 @@
 
 ;; shell mode
 (defun sh ()
+  "Launch an ANSI term with /bin/bash"
   (interactive)
   (ansi-term "/bin/bash"))
 
@@ -14,7 +28,10 @@
 (defun kill-other-buffers ()
   "Kill all other buffers"
   (interactive)
-  (mapc 'kill-buffer (delq (current-buffer) (buffer-list))))
+  (mapc 'kill-buffer (delq (current-buffer) (buffer-list)))
+  (message "ALL OTHER BUFFERS KILLED"))
+
+(global-set-key [(f5)] 'kill-other-buffers)
 
 ;; kill all dired buffers
 (defun kill-all-dired-buffers()
